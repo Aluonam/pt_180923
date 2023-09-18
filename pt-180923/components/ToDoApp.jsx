1fb3e074cb12a32488f6bd4e062c4601f3010a58
Paula Cano <paulacanoroman@gmail.com>
2023-09-18 23:01:44 +0200

    feat: borrar tarea del LocalStorage

--- pt-180923/components/ToDoApp.jsx
+++ pt-180923/components/ToDoApp.jsx
@@ -4,24 +4,25 @@ import ToDoList from './ToDoList'
 const ToDoApp = () => {
 
     const [taskList, setTaskList] = useState([])
-    const [inputUserData, setInputUIserData] = useState("")
+    const [inputUserData, setInputUserData] = useState("")
 
     useEffect(() => {
         const taskLocal = JSON.parse(localStorage.getItem("listOfTasks"))
         if (taskLocal !== null)setTaskList(taskLocal)
+        
     }, [])
     
     const addTaksListToLocal = (value)=>{localStorage.setItem("listOfTasks",JSON.stringify(value))}
 
     const handleAddTask = ()=>{
         setTaskList([...taskList,inputUserData])
-        setInputUIserData("")
+        setInputUserData("")
         addTaksListToLocal([...taskList,inputUserData])
     }
 
   return (
     <>
-    <input type='text' onChange={(e)=>{setInputUIserData(e.target.value)}} value={inputUserData}></input>
+    <input type='text' onChange={(e)=>{setInputUserData(e.target.value)}} value={inputUserData}></input>
     <button onClick={handleAddTask}>Send</button>
     <ToDoList taskList={taskList} setTaskList={setTaskList}></ToDoList>
     </>
--- pt-180923/components/ToDoList.jsx
+++ pt-180923/components/ToDoList.jsx
@@ -6,18 +6,19 @@ const ToDoList = ({taskList, setTaskList}) => {
     const deleteTask = (actualElement) => {
         const newTaskList = taskList.filter((item)=>item !== actualElement)
         setTaskList(newTaskList)
-        
+        localStorage.setItem("listOfTasks",JSON.stringify(newTaskList))
+
     }
 
-
-    const list = taskList.map((actualElement=>{
+    const list = taskList.map((actualElement, index)=>{
         return(
         <ul>
             <ToDoItem actualTask={actualElement}></ToDoItem>
             <button onClick={()=>{deleteTask(actualElement)}}>Eliminar</button>
         </ul>
         )
-    }))
+    })
+
   return (
     <div>{list}</div>
   )
